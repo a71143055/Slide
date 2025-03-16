01fd998c9b0a21ed7f76bc277491286f461b955e
a71143055 <a71143055@gmail.com>
2025-03-16 18:09:45 +0900

    Slide

---
+++ src/main/java/kr/ac/kopo/jeong/slide/SlideApplication.java
@@ -0,0 +1,13 @@
+package kr.ac.kopo.jeong.slide;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlideApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SlideApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/ac/kopo/jeong/slide/SlideApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.ac.kopo.jeong.slide;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlideApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
